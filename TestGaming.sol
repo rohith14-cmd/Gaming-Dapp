a64e4579bb300016992011bab2dcb2fa28b78f34
rohith14-cmd <56895994+rohith14-cmd@users.noreply.github.com>
2021-02-10 00:08:56 +0530

    Add files via upload

---
+++ TestGaming.sol
@@ -0,0 +1,42 @@
+pragma solidity 0.5.*;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Gaming.sol";
+
+contract TestGaming {
+    uint public initialBalance = 10 ether;
+    Gaming gaming;
+
+    function beforeAll() public {
+        gaming = Gaming(DeployedAddresses.Gaming());
+    }
+
+    function testPlayerWonGuessHigher() public {
+        bool expected = true;
+        bool result = gaming.determineWinner(5, 4, true);
+
+        Assert.equal(expected, result, "The player should have won by guessing the mystery number was higher than their number");
+    }
+
+    function testPlayerWonGuessLower() public {
+        bool expected = true;
+        bool result = gaming.determineWinner(5, 6, false);
+
+        Assert.equal(expected, result, "The player should have won by guessing the mystery number was lower than their number");
+    }
+
+    function testPlayerLostGuessLower() public {
+        bool expected = false;
+        bool result = gaming.determineWinner(5, 4, false);
+
+        Assert.equal(expected, result, "The player should have lost by guessing the mystery number was lower than their number");
+    }
+
+    function testPlayerLostGuessHigher() public {
+        bool expected = false;
+        bool result = gaming.determineWinner(5, 6, true);
+
+        Assert.equal(expected, result, "The player should have lost by guessing the mystery number was higher than their number");
+    }
+}
